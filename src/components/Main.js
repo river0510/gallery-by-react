b234db7fc8379aab82e38605606d62ae57a9a230
黄河 <huanghe@huanghedeMacBook-Pro.local>
2017-02-20 19:26:39 +0800

    init picture

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,29 @@ require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片相关数据，利用自执行函数 将图片信息转化为url信息
+var imageDatas = require('../data/imageDatas.json');
+
+imageDatas = (function genImageUrl (imageArr){
+	for(let i=0; i<imageArr.length; i++){
+		var everyImage = imageArr[i];
+		everyImage.imageUrl = require('../images/'+everyImage.fileName);
+		imageArr[i] = everyImage;
+	}
+	return imageArr;
+})(imageDatas);
+
+// imageDatas = genImageUrl(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">hello</div>
-      </div>
+      <section className="stage">
+      	<section className="img-sec">
+      	</section>
+      	<nav className="controller-nav">
+      	</nav>
+      </section>
     );
   }
 }
